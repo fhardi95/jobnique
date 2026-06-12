de231acd4491c0132e8a8f2e7d7da1e866651e7a
fhardi95 <fhardi.said@gmail.com>
2026-06-12 03:51:07 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -33,14 +33,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ads.txt",
-        destination: "/api/ads-txt",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
