a078cdb5e9dc27a3992331e9870dd57ae3076ebe
fhardi95 <fhardi.said@gmail.com>
2026-06-12 02:03:11 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/ads.txt") {
+    return NextResponse.rewrite(new URL("/api/ads-txt", request.url));
+  }
+
   const response = NextResponse.next();
   response.headers.set("x-url", request.url);
   return response;
